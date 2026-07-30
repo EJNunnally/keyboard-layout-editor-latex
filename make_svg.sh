9df3530c6b9274ba4a80fa58ce0f9da5f610523c
erik <ejnunnally@gmail.com>
2026-07-29 19:00:38 -0700

    added images without fonts

--- make_svg.sh
+++ make_svg.sh
@@ -2,7 +2,8 @@
 if [ -z "$1" ]; then
     OUTPUT="images/$(date +%Y-%m-%d_%H.%M.%S).svg"
 else
-    OUTPUT="images/$1.svg"
+    OUTPUT="images-no-fonts/$1.svg"
 fi
 lualatex --output-format=dvi main.tex
-dvisvgm --font-format=woff2 main.dvi --output=$OUTPUT
+# dvisvgm --font-format=woff2 main.dvi --output=$OUTPUT
+dvisvgm --no-fonts main.dvi --output=$OUTPUT
